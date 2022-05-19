bf24273e84b04de330196fd3211c1b323f701b2e
Komatsu <KomatsuK@SI.EDU>
2022-05-19 17:08:24 -0400

    Import data. Notes on analyses.

--- conSME_community_composition.R
+++ conSME_community_composition.R
@@ -1,14 +1,20 @@
 library(tidyverse)
 
-setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\konza projects\\herbivore removal plots\\data\\species composition')
+setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\konza projects\\conSME\\data\\species composition') #laptop
+setwd('C:\\Users\\komatsuk\\Dropbox (Smithsonian)\\konza projects\\conSME\\data\\species composition') #desktop
+
 
 ###functions
 `%notin%` <- Negate(`%in%`)
 
 sp2018 <- read.csv('ConSME_species composition_2018.csv')
 sp2019 <- read.csv('ConSME_species composition_2019.csv')
+sp2020 <- read.csv('ConSME_species composition_2020.csv')%>%
+  select(-X, -taxa)
+sp2021 <- read.csv('ConSME_species composition_2021.csv')%>%
+  select(-taxa, -flw_cover, -flw_number)
 
-spAll <- rbind(sp2018,sp2019)%>%
+spAll <- rbind(sp2018,sp2019,sp2020,sp2021)%>%
   group_by(year, watershed, block, plot, sppnum)%>%
   summarise(max_cover=max(cover))%>%
   ungroup()%>%
@@ -16,29 +22,55 @@ spAll <- rbind(sp2018,sp2019)%>%
   filter(gen %notin% c('litter', 'rock', 'dung', 'bare_ground', 'bison_trail'))
 
 
-#finding the most frequent species across all plots and years for updating the datasheets
-freq <- spAll%>%
-  mutate(genus_species=paste(genus,species, sep='_'))%>%
-  filter(max_cover>0, genus_species!='NA_NA')%>%
-  group_by(watershed, block, plot, sppnum, genus_species, lifeform)%>%
-  summarize(cover=mean(max_cover))%>%
-  ungroup()%>%
-  group_by(sppnum, genus_species, lifeform, watershed)%>%
-  summarise(freq=length(genus_species))%>%
-  ungroup()%>%
-  spread(key=watershed, value=freq)
+#to do: richness, evenness, comp diff, spp turnover?, trends for dominant spp, simper for which forbs come to dominate (same in bison vs invert areas?)
 
-# write.csv(freq, 'species_frequency_2018-2019.csv', row.names=F)
 
-#finding the most frequent species across all plots and years for updating the datasheets
-abund <- spAll%>%
-  mutate(genus_species=paste(genus,species, sep='_'))%>%
-  group_by(watershed, block, plot, sppnum, genus_species, lifeform)%>%
-  summarize(cover=mean(max_cover))%>%
-  ungroup()%>%
-  filter(cover>0, genus_species!='NA_NA')%>%
-  group_by(sppnum, genus_species, lifeform, watershed)%>%
-  summarise(freq=length(genus_species), avg_cover=mean(cover))%>%
-  ungroup()
 
-# write.csv(abund, 'conSME_species_dominance_2018-2019.csv', row.names=F)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #finding the most frequent species across all plots and years for updating the datasheets
+# freq <- spAll%>%
+#   mutate(genus_species=paste(genus,species, sep='_'))%>%
+#   filter(max_cover>0, genus_species!='NA_NA')%>%
+#   group_by(watershed, block, plot, sppnum, genus_species, lifeform)%>%
+#   summarize(cover=mean(max_cover))%>%
+#   ungroup()%>%
+#   group_by(sppnum, genus_species, lifeform, watershed)%>%
+#   summarise(freq=length(genus_species))%>%
+#   ungroup()%>%
+#   spread(key=watershed, value=freq)
+# 
+# # write.csv(freq, 'species_frequency_2018-2019.csv', row.names=F)
+# 
+# #finding the most frequent species across all plots and years for updating the datasheets
+# abund <- spAll%>%
+#   mutate(genus_species=paste(genus,species, sep='_'))%>%
+#   group_by(watershed, block, plot, sppnum, genus_species, lifeform)%>%
+#   summarize(cover=mean(max_cover))%>%
+#   ungroup()%>%
+#   filter(cover>0, genus_species!='NA_NA')%>%
+#   group_by(sppnum, genus_species, lifeform, watershed)%>%
+#   summarise(freq=length(genus_species), avg_cover=mean(cover))%>%
+#   ungroup()
+# 
+# # write.csv(abund, 'conSME_species_dominance_2018-2019.csv', row.names=F)
