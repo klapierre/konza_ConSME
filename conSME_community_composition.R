5178eee49786db37faa82f0c7662a790f720be03
Komatsu <KomatsuK@SI.EDU>
2022-05-20 17:15:45 -0400

    Figures for community metrics, NMDS, permanova and permdisp, simper, dom spp trends.

--- conSME_community_composition.R
+++ conSME_community_composition.R
@@ -8,6 +8,7 @@
 library(codyn)
 library(nlme)
 library(emmeans)
+library(vegan)
 library(tidyverse)
 
 setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\konza projects\\conSME\\data\\species composition') #laptop
@@ -62,7 +63,8 @@ spAll <- rbind(sp2018,sp2019,sp2020,sp2021)%>%
   summarise(max_cover=max(cover))%>%
   ungroup()%>%
   left_join(read.csv('PPS011_new KNZ spp list.csv'))%>%
-  filter(gen %notin% c('litter', 'rock', 'dung', 'bare_ground', 'bison_trail'))
+  filter(gen %notin% c('litter', 'rock', 'dung', 'bare_ground', 'bison_trail'))%>%
+  mutate(genus_species=paste(genus, species, sep='_'))
 
 
 ##### relative cover #####
@@ -75,7 +77,9 @@ relCover <- spAll%>%
   left_join(totCover)%>%
   mutate(rel_cover=100*(max_cover/total_cover))%>% #calculate relative cover
   select(-total_cover)%>%
-  mutate(replicate=paste(watershed, block, plot, sep='::'))
+  mutate(replicate=paste(watershed, block, plot, sep='::'))%>%
+  left_join(trt)%>%
+  filter(!is.na(sppnum)) #remove 5 entries that were unknowns
 
 
 ##### community metrics #####
@@ -84,25 +88,163 @@ commMetrics <- community_structure(relCover, time.var='year', abundance.var='rel
   mutate(plot=as.integer(plot))%>%
   left_join(trt)
 
-#######START HERE
+##### richness response #####
 summary(richModel <- lme(richness~watershed*trt*year,
-                               data=biomassLater,
+                               data=commMetrics,
                                random=~1|block/trt,
                                correlation=corCompSymm(form=~year|block/trt), 
                                control=lmeControl(returnObject=T)))
-anova.lme(totBiomassModel, type='sequential') 
-emmeans(totBiomassModel, pairwise~year*trt, adjust="tukey")
+anova.lme(richModel, type='sequential') 
+emmeans(richModel, pairwise~year*trt, adjust="tukey")
 
+#figure - richness by year
+ggplot(data=barGraphStats(data=commMetrics, variable="richness", byFactorNames=c("trt", "year")), aes(x=trt, y=mean)) +
+  geom_bar(position=position_dodge(0.1), size=2, stat="identity", color='black', fill='white') +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=position_dodge(0.1), size=2) +
+  ylab(expression(paste('Total Biomass (g m'^'-2',')'))) +
+  scale_x_discrete(limits=c('BSI', 'BSX', 'XSI', 'XXI', 'XSX', 'XXX')) +
+  theme(axis.title.x=element_blank(), axis.text.x=element_text(size=30, angle=90), axis.title.y=element_text(size=30, angle=90, vjust=1, margin=margin(r=15)), axis.text.y=element_text(size=26), legend.position=c(0, 1), legend.justification=c(0,1), strip.text=element_text(size=30)) +
+  # coord_cartesian(ylim=c(0,750)) +
+  facet_grid(cols=vars(year))
+#export at 1400x600
+
+
+
+##### evenness response #####
+summary(evarModel <- lme(Evar~watershed*trt*year,
+                         data=subset(commMetrics, year>2018),
+                         random=~1|block/trt,
+                         correlation=corCompSymm(form=~year|block/trt), 
+                         control=lmeControl(returnObject=T)))
+anova.lme(evarModel, type='sequential') 
+emmeans(evarModel, pairwise~year*trt*watershed, adjust="tukey")
+
+anova.lme(evarModel <- lme(Evar~trt,
+                           data=subset(commMetrics, year==2021 & watershed=='N4B'),
+                           random=~1|block/trt))
+emmeans(evarModel, pairwise~trt, adjust="tukey")
+
+#figure - evenness by year
+ggplot(data=barGraphStats(data=subset(commMetrics, year>2018), variable="Evar", byFactorNames=c("trt", "year", "watershed")), aes(x=trt, y=mean)) +
+  geom_bar(position=position_dodge(0.1), size=2, stat="identity", color='black', fill='white') +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=position_dodge(0.1), size=2) +
+  ylab(expression(paste('Evenness'))) +
+  scale_x_discrete(limits=c('BSI', 'BSX', 'XSI', 'XXI', 'XSX', 'XXX')) +
+  theme(axis.title.x=element_blank(), axis.text.x=element_text(size=30, angle=90), axis.title.y=element_text(size=30, angle=90, vjust=1, margin=margin(r=15)), axis.text.y=element_text(size=26), legend.position=c(0, 1), legend.justification=c(0,1), strip.text=element_text(size=30)) +
+  coord_cartesian(ylim=c(0,0.55)) +
+  facet_grid(cols=vars(year), rows=vars(watershed))
+#export at 1400x1200
 
 
 
 ##### community difference #####
+commDiff <- multivariate_difference(df=relCover, time.var='year', species.var='genus_species', abundance.var='rel_cover', replicate.var='replicate', treatment.var='trt', reference.treatment='BSI')
+
+ggplot(data=subset(commDiff, year>2018), aes(x=year, y=composition_diff, color=trt2)) +
+  geom_point() +
+  geom_smooth(method='lm', formula=y~poly(x,2), se=F)
 
 
 
+##### PERMANOVA #####
+relCover2021 <- relCover%>%
+  select(year, replicate, trt, genus_species, rel_cover)%>%
+  pivot_wider(names_from='genus_species', values_from='rel_cover', values_fill=list(rel_cover=0))%>%
+  filter(year==2021)
+
+print(permanova <- adonis2(formula = relCover2021[,4:183]~trt, data=relCover2021, permutations=999, method="bray"))
+#F=3.487, df=5,104, p=0.001
+
+#betadisper
+veg <- vegdist(relCover2021[,4:183], method = "bray")
+dispersion <- betadisper(veg, relCover2021$trt)
+permutest(dispersion, pairwise=TRUE, permutations=999) 
+#F=3.982, df=5,99, p=0.005
+
+sppBC <- metaMDS(relCover2021[,4:183])
+
+plotData <- relCover2021[,2:3]
+
+#Use the vegan ellipse function to make ellipses
+veganCovEllipse<-function (cov, center = c(0, 0), scale = 1, npoints = 100)
+{
+  theta <- (0:npoints) * 2 * pi/npoints
+  Circle <- cbind(cos(theta), sin(theta))
+  t(center + scale * t(Circle %*% chol(cov)))
+}
+
+BC_NMDS = data.frame(MDS1 = sppBC$points[,1], MDS2 = sppBC$points[,2],group=relCover2021$trt)
+BC_NMDS_Graph <- cbind(plotData,BC_NMDS)
+BC_Ord_Ellipses<-ordiellipse(sppBC, plotData$trt, display = "sites",
+                             kind = "se", conf = 0.95, label = T)               
+
+ord3 <- data.frame(plotData,scores(sppBC,display="sites"))%>%
+  group_by(trt)
+
+BC_Ord_Ellipses<-ordiellipse(sppBC, plotData$trt, display = "sites",
+                             kind = "se", conf = 0.95, label = T)
+BC_Ellipses <- data.frame() #Make a new empty data frame called BC_Ellipses  
+for(g in unique(BC_NMDS$group)){
+  BC_Ellipses <- rbind(BC_Ellipses, cbind(as.data.frame(with(BC_NMDS[BC_NMDS$group==g,], 
+                                                             veganCovEllipse(BC_Ord_Ellipses[[g]]$cov,BC_Ord_Ellipses[[g]]$center,BC_Ord_Ellipses[[g]]$scale)))
+                                          ,group=g))
+} #Generate ellipses points
+
+ggplot(BC_NMDS_Graph, aes(x=MDS1, y=MDS2, color=group,linetype = group, shape = group)) +
+  geom_point(size=6)+ 
+  geom_path(data = BC_Ellipses, aes(x = NMDS1, y = NMDS2), size = 3) +
+  labs(color="", linetype = "", shape = "") +
+  scale_colour_manual(values=c("brown", "brown", "dark green", "dark green", "dark green", "dark green"), name = "") +
+  scale_linetype_manual(values = c("twodash", "solid", "twodash", "solid", "twodash", "solid"), name = "") +
+  xlab("NMDS1")+ 
+  ylab("NMDS2")+ 
+  theme(axis.text.x=element_text(size=24, color = "black"), axis.text.y = element_text(size = 24, color = "black"), legend.text = element_text(size = 24))
 
 
-#to do: richness, evenness, comp diff, spp turnover?, trends for dominant spp, simper for which forbs come to dominate (same in bison vs invert areas?)
+
+##### simper #####
+print(sim <- with(relCover2021, simper(relCover2021[,4:183], trt)))
+
+
+##### trends for dominant species #####
+ggplot(barGraphStats(data=subset(relCover, genus_species=='andropogon_gerardii' & year>2018), variable="max_cover", byFactorNames=c("year", "trt", "watershed")), aes(x=year, y=mean, color=trt)) +
+  geom_point(size=3) +
+  geom_smooth(method='lm', formula=y~poly(x,2), se=F) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.1) +
+  ylab('Andropogon gerardii cover') +
+  facet_wrap(~watershed)
+
+ggplot(barGraphStats(data=subset(relCover, genus_species=='bouteloua_curtipendula' & year>2018), variable="max_cover", byFactorNames=c("year", "trt", "watershed")), aes(x=year, y=mean, color=trt)) +
+  geom_point(size=3) +
+  geom_smooth(method='lm', formula=y~poly(x,2), se=F) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.1) +
+  ylab('Bouteloua curtipendula cover') +
+  facet_wrap(~watershed)
+
+ggplot(barGraphStats(data=subset(relCover, genus_species=='lespedeza_violacea' & year>2018), variable="max_cover", byFactorNames=c("year", "trt", "watershed")), aes(x=year, y=mean, color=trt)) +
+  geom_point(size=3) +
+  geom_smooth(method='lm', formula=y~poly(x,2), se=F) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.1) +
+  ylab('Lespedeza violacea cover') +
+  facet_wrap(~watershed)
+
+ggplot(barGraphStats(data=subset(relCover, genus_species=='ambrosia_psilostachya' & year>2018), variable="max_cover", byFactorNames=c("year", "trt", "watershed")), aes(x=year, y=mean, color=trt)) +
+  geom_point(size=3) +
+  geom_smooth(method='lm', formula=y~poly(x,2), se=F) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.1) +
+  ylab('Ambrosia psilostachya cover') +
+  facet_wrap(~watershed)
+
+ggplot(barGraphStats(data=subset(relCover, genus_species=='oxalis_violacea' & year>2018), variable="max_cover", byFactorNames=c("year", "trt", "watershed")), aes(x=year, y=mean, color=trt)) +
+  geom_point(size=3) +
+  geom_smooth(method='lm', formula=y~poly(x,2), se=F) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.1) +
+  ylab('Oxalis violacea cover') +
+  facet_wrap(~watershed)
+
+
+
+#spp turnover?
 
 
 
